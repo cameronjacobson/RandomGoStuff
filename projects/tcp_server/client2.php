2a7921aa0b0bf8552f6c2f5c11134fa87c83b9c2
Cameron Jacobson <cameron@datashovel.com>
2013-05-19 09:54:14 +0000

    removed invalid comment from client2.php

--- projects/tcp_server/client2.php
+++ projects/tcp_server/client2.php
@@ -1,10 +1,5 @@
 <?php
 
-// Usage:  php client.php [randomword]
-//
-// will run 5 clients sequentially.  To simulate multiple
-// clients, just start up multiple client processes
-
 for($a=0;$a<10;$a++){
 	$client = stream_socket_client('tcp://127.0.0.1:9201');
 	fwrite($client,$argv[1].' '.$x.PHP_EOL);
